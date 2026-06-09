4be8bac3cb00480dfa858495f4311300d29a1bad
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-09 21:49:25 +0530

    Setup TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
   return (
-    <>
+    <div className="w-full h-screen bg-slate-400 flex flex-wrap items-center justify-center">
       Mega Blog App with Appwrite
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
