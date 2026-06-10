92ddb19615511da45f75dd9f43d8ac58ba8c2c85
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-10 21:06:13 +0530

    Define Logo Component

---
+++ src/components/shared/Logo.jsx
@@ -0,0 +1,13 @@
+import logoCompact from "../assets/logo-full.svg";
+import logoFull from "../assets/logo-full.svg";
+
+function Logo({ type = "full", width = "100px" }) {
+  const logoSrc = type === "full" ? logoFull : logoCompact;
+  return (
+    <div>
+      <img src={logoSrc} alt="MB logo" width={width} />
+    </div>
+  );
+}
+
+export default Logo;
