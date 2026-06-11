2c6c5e4c4ff979b05d53faffb363d1ef782b09c9
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-11 21:39:49 +0530

    Update Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,73 @@
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+
+import LogoutBtn from "./LogoutBtn";
+import Container from "../shared/Container";
+import Logo from "../shared/Logo";
+
 function Header() {
-  return <div>Header</div>;
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container className="flex justify-between items-center">
+        <div className="">
+          <Link to="/">
+            <Logo type="compact" width="200px" />
+          </Link>
+        </div>
+        <nav className="flex">
+          <ul className="flex gap-3">
+            {navItems?.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    onClick={() => navigate(item.slug)}
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
