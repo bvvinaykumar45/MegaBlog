5d7bea188239eda78c15e7f36003b56c92f108fb
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-13 21:25:43 +0530

    Setup TinyMCE API Key Env variable

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ const config = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  tinyMceApiKey: String(import.meta.env.VITE_TINY_MCE_API_KEY),
 };
 
 export default config;
