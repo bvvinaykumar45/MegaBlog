30a6242ea9dc1452f86b6d33fdf92f794688b968
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-06-14 10:44:26 +0530

    Define & Setup HomePage Route

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,9 +5,12 @@ import {
 } from "react-router";
 
 import App from "./App";
+import { Home } from "./pages";
 
 const routes = createRoutesFromElements(
-  <Route path="/" Component={App}></Route>
+  <Route path="/" Component={App}>
+    <Route index Component={Home} />
+  </Route>
 );
 
 const browserRouter = createBrowserRouter(routes);
